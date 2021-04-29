3550e5eaeaa4ba522aa0ef2cd359c2a7021c9938
v-king777 <v.kolpashchikov@gmail.com>
2021-04-29 21:30:04 +0300

    new file:   Task_12_1_5/Advertising.cs

---
+++ Task_12_1_5/Advertising.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace Task_12_1_5
+{
+    public class Advertising
+    {
+        public void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com!");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда!");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу!");
+            Thread.Sleep(3000);
+        }
+    }
+}
