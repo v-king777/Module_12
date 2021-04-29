8ecaf42ec0be1c45a949ad6bf9e52aca6a4a4d70
v-king777 <v.kolpashchikov@gmail.com>
2021-04-29 21:30:19 +0300

    new file:   Task_12_1_5/ListOfUsers.cs

---
+++ Task_12_1_5/ListOfUsers.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Task_12_1_5
+{
+    public class ListOfUsers
+    {
+        private List<User> users = new List<User>();
+
+        public List<User> GetUsers()
+        {
+            users.Add(new User { Login = "v-king777", Name = "Владимир", IsPremium = true });
+            users.Add(new User { Login = "alfa", Name = "Артём", IsPremium = false });
+            users.Add(new User { Login = "beta", Name = "Борис", IsPremium = true });
+            users.Add(new User { Login = "gamma", Name = "Геннадий", IsPremium = false });
+            users.Add(new User { Login = "delta", Name = "Дмитрий", IsPremium = true });
+
+            return users;
+        }
+    }
+}
