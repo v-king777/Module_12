05dca5d2fc529209d09b41e948efacc09083e23d
v-king777 <v.kolpashchikov@gmail.com>
2021-04-29 21:31:15 +0300

    new file:   Task_12_1_5/User.cs

---
+++ Task_12_1_5/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Task_12_1_5
+{
+    public class User
+    {
+        public string Login { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsPremium { get; set; }
+    }
+}
