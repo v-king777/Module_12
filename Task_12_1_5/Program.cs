e851a35ed8af7cc8eea4a423aa9f83abfb4234bf
v-king777 <v.kolpashchikov@gmail.com>
2021-04-29 21:30:37 +0300

    modified:   Task_12_1_5/Program.cs

--- Task_12_1_5/Program.cs
+++ Task_12_1_5/Program.cs
@@ -6,7 +6,20 @@ namespace Task_12_1_5
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ListOfUsers listOfUsers = new ListOfUsers();
+            Advertising advertising = new Advertising();
+
+            foreach (var user in listOfUsers.GetUsers())
+            {
+                Console.WriteLine("Привет, {0}!", user.Name);
+
+                if (!user.IsPremium)
+                {
+                    advertising.ShowAds();
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
