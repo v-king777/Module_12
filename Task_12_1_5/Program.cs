c7d171cdcf4e995e8e844dc7015bbd4990e10823
v-king777 <v.kolpashchikov@gmail.com>
2021-04-29 19:08:25 +0300

    Добавьте файлы проекта.

---
+++ Task_12_1_5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task_12_1_5
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
